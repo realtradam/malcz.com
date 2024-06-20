62904be825a84ecac6b43c09ad00173955328555
realtradam <github@tradam.dev>
2024-06-19 21:43:38 -0400

    add a bit more spacing on main page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,11 +4,11 @@ export default function Home () {
 	return(
 		<>
 		<div>
-		<div className="flex flex-col gap-16 max-w-6xl shrink">
-			<div className="title font-bold text-6xl font-title">Welcome</div>
+		<div className="flex flex-col gap-16 max-w-16xl shrink">
+			<div className="title font-bold text-6xl font-title mb-32">Welcome</div>
 			<div className="">
 				<div className="jumbotron jumbotron-fluid bg-transparent">
-					<div className="container secondary-color bg-stone-950 prose prose-invert marker:text-stone-50 p-8 rounded-xl">
+					<div className="container secondary-color bg-stone-950 prose prose-invert marker:text-stone-50 p-16 rounded-xl">
 					<HomeMD />
 					</div>
 				</div>
