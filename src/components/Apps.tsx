2944a5609d478c7a409579b4fcc5bf5c841037c9
realtradam <github@tradam.dev>
2024-06-19 21:29:02 -0400

    fix typing issues

--- src/components/Apps.tsx
+++ src/components/Apps.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 //import { Link } from "react-router-dom";
 //import GameCard from "./GameCard";
 //import Button from "./Button";
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ReactComponent as HomeMD } from '../md/main.md';
 
 export default function Home () {
